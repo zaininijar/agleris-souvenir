2d738bf824b4b4be685b401cfec166b86432b4e5
zaininijar <zaininijar7610@gmail.com>
2024-01-28 04:51:29 +0700

    dashboard overview done

--- view/admin/index.php
+++ view/admin/index.php
@@ -2,10 +2,12 @@
 <?php require_once 'utils/format.php' ?>
 <?php
 
+$sql_transaction_count_by_status = "SELECT COUNT(transactions.id) AS transaction_count, transactions.status AS transaction_status FROM transactions GROUP BY transactions.status";
+$result_transaction_count_by_status = $conn->query($sql_transaction_count_by_status)->fetch_all();
+
 $sql_report = "SELECT
                   SUM(transactions.price_total) AS purchase_count,
-                  COUNT(transactions.id) AS transaction_count,
-                  COUNT(transactions.id) AS transaction_rejected_count
+                  COUNT(transactions.id) AS transaction_count
                   FROM transactions
                   WHERE transactions.status = 2";
 
@@ -79,12 +81,8 @@ $transaction_rejected_count = $report_transaction_rejected['transaction_rejected
                                 role="tab" aria-controls="overview" aria-selected="true">Overview</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="sales-tab" data-bs-toggle="tab" href="#sales" role="tab"
-                                aria-controls="sales" aria-selected="false">Sales</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="purchases-tab" data-bs-toggle="tab" href="#purchases" role="tab"
-                                aria-controls="purchases" aria-selected="false">Purchases</a>
+                            <a class="nav-link" id="transaksi-tab" data-bs-toggle="tab" href="#transaksi" role="tab"
+                                aria-controls="transaksi" aria-selected="true">Detail Transaksi</a>
                         </li>
                     </ul>
                     <div class="tab-content py-0 px-0">
@@ -101,18 +99,18 @@ $transaction_rejected_count = $report_transaction_rejected['transaction_rejected
                                 </div>
                                 <div
                                     class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-chair-school me-3 icon-lg text-danger"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Total Transaksi</small>
-                                        <h5 class="me-2 mb-0"><?= $transaction_count ?></h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
                                     <i class="mdi mdi-table me-3 icon-lg text-success"></i>
                                     <div class="d-flex flex-column justify-content-around">
                                         <small class="mb-1 text-muted">Total Product</small>
                                         <h5 class="me-2 mb-0"><?= $souvenir_count ?></h5>
+                                    </div>
+                                </div>
+                                <div
+                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-chair-school me-3 icon-lg text-success"></i>
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <small class="mb-1 text-muted">Total Transaksi Berhasil</small>
+                                        <h5 class="me-2 mb-0"><?= $transaction_count ?></h5>
                                     </div>
                                 </div>
                                 <div
@@ -125,112 +123,38 @@ $transaction_rejected_count = $report_transaction_rejected['transaction_rejected
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="sales" role="tabpanel" aria-labelledby="sales-tab">
+                        <div class="tab-pane fade" id="transaksi" role="tabpanel" aria-labelledby="transaksi-tab">
                             <div class="d-flex flex-wrap justify-content-xl-between">
                                 <div
-                                    class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-calendar-heart icon-lg me-3 text-primary"></i>
+                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
+                                    <i class="mdi mdi-chair-school me-3 icon-lg text-secondary"></i>
                                     <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Start date</small>
-                                        <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle p-0 bg-transparent border-0 text-dark shadow-none font-weight-medium"
-                                                href="#" role="button" id="dropdownMenuLinkA" data-bs-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false">
-                                                <h5 class="mb-0 d-inline-block">26 Jul 2018</h5>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLinkA">
-                                                <a class="dropdown-item" href="#">12 Aug 2018</a>
-                                                <a class="dropdown-item" href="#">22 Sep 2018</a>
-                                                <a class="dropdown-item" href="#">21 Oct 2018</a>
-                                            </div>
-                                        </div>
+                                        <small class="mb-1 text-muted">
+
+                                            <?= $result_transaction_count_by_status[0][1] ?>
+                                        </small>
+                                        <h5 class="me-2 mb-0"><?= $result_transaction_count_by_status[0][0] ?></h5>
                                     </div>
                                 </div>
                                 <div
                                     class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-download me-3 icon-lg text-warning"></i>
+                                    <i class="mdi mdi-chair-school me-3 icon-lg text-info"></i>
                                     <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Downloads</small>
-                                        <h5 class="me-2 mb-0">2233783</h5>
+                                        <small class="mb-1 text-muted">
+
+                                            <?= $result_transaction_count_by_status[4][1] ?>
+                                        </small>
+                                        <h5 class="me-2 mb-0"><?= $result_transaction_count_by_status[4][0] ?></h5>
                                     </div>
                                 </div>
                                 <div
                                     class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-eye me-3 icon-lg text-success"></i>
+                                    <i class="mdi mdi-chair-school me-3 icon-lg text-warning"></i>
                                     <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Total views</small>
-                                        <h5 class="me-2 mb-0">9833550</h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-currency-usd me-3 icon-lg text-danger"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Revenue</small>
-                                        <h5 class="me-2 mb-0">$577545</h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-flag me-3 icon-lg text-danger"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Flagged</small>
-                                        <h5 class="me-2 mb-0">3497843</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tab-pane fade" id="purchases" role="tabpanel" aria-labelledby="purchases-tab">
-                            <div class="d-flex flex-wrap justify-content-xl-between">
-                                <div
-                                    class="d-none d-xl-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-calendar-heart icon-lg me-3 text-primary"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Start date</small>
-                                        <div class="dropdown">
-                                            <a class="btn btn-secondary dropdown-toggle p-0 bg-transparent border-0 text-dark shadow-none font-weight-medium"
-                                                href="#" role="button" id="dropdownMenuLinkA" data-bs-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false">
-                                                <h5 class="mb-0 d-inline-block">26 Jul 2018</h5>
-                                            </a>
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLinkA">
-                                                <a class="dropdown-item" href="#">12 Aug 2018</a>
-                                                <a class="dropdown-item" href="#">22 Sep 2018</a>
-                                                <a class="dropdown-item" href="#">21 Oct 2018</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-currency-usd me-3 icon-lg text-danger"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Revenue</small>
-                                        <h5 class="me-2 mb-0">$577545</h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-eye me-3 icon-lg text-success"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Total views</small>
-                                        <h5 class="me-2 mb-0">9833550</h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-download me-3 icon-lg text-warning"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Downloads</small>
-                                        <h5 class="me-2 mb-0">2233783</h5>
-                                    </div>
-                                </div>
-                                <div
-                                    class="d-flex py-3 border-md-right flex-grow-1 align-items-center justify-content-center p-3 item">
-                                    <i class="mdi mdi-flag me-3 icon-lg text-danger"></i>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <small class="mb-1 text-muted">Flagged</small>
-                                        <h5 class="me-2 mb-0">3497843</h5>
+                                        <small class="mb-1 text-muted">
+                                            <?= $result_transaction_count_by_status[3][1] ?>
+                                        </small>
+                                        <h5 class="me-2 mb-0"><?= $result_transaction_count_by_status[3][0] ?></h5>
                                     </div>
                                 </div>
                             </div>
